157b359efbd3bbf856bca0f27587779f3e1ac9f5
askldfghj <askldfghj@gmail.com>
2020-04-15 22:48:23 +0900

    enable databinding, input dark theme color

--- app/src/main/java/com/askldfghj/darkthemesample/MainActivity.kt
+++ app/src/main/java/com/askldfghj/darkthemesample/MainActivity.kt
@@ -2,11 +2,15 @@ package com.askldfghj.darkthemesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.askldfghj.darkthemesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).also {
+
+        }
     }
 }
